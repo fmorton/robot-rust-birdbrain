f47c794cab685f55e5e5186008870e36c949ac1b
Frank Morton <fmorton@mac.com>
2025-12-29 20:03:18 -0500

    split out request.rs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,16 @@
+mod request;
+
 use std::error::Error;
-
-struct Request {
-    body: String,
-    status: u16,
-}
-
-impl Request {
-    fn request_uri_from_vector(request: &[&str]) -> String {
-        return "http://127.0.0.1:30061/".to_string() + &request.join("/")
-    }
-
-    fn response_from_uri(uri: &String) -> Request {
-        let response = reqwest::blocking::get(uri);
-
-        match response {
-            Ok(response) => {
-                if response.status().is_success() {
-                    let status = response.status().as_u16();
-
-                    match response.text() {
-                        Ok(text) => return Request { body: text, status: status },
-                        Err(e) => eprintln!("Error reading body: {}", e),
-                    }
-                } else {
-                    eprintln!("API returned non-success status: {}", response.status());
-                }
-            },
-            Err(err) => {
-                eprintln!("Request failed: {}", err);
-            }
-        }
-
-        Request { body: "".to_string(), status: 500 }
-    }
-
-    fn response(request: &[&str]) -> Request {
-        let uri = Request::request_uri_from_vector(request).to_string();
-
-        return Request::response_from_uri(&uri)
-    }
-}
+use crate::request::Request;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let uri = Request::request_uri_from_vector(&vec!["hummingbird", "in", "orientation", "Shake", "A"]);
+    //let uri = Request::request_uri_from_vector(&vec!["hummingbird", "in", "orientation", "Shake", "A"]);
 
-    let response_using_uri = Request::response_from_uri(&uri);
+    //let response_using_uri = Request::response_from_uri(&uri);
 
-    println!("URI: {}", uri);
-    println!("Status: {}", response_using_uri.status);
-    println!("Body: {}", response_using_uri.body);
+    //println!("URI: {}", uri);
+    //println!("Status: {}", response_using_uri.status);
+    //println!("Body: {}", response_using_uri.body);
 
     let response = Request::response(&vec!["hummingbird", "in", "orientation", "Shake", "A"]);
 
